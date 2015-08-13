8a9e32c4223fd1be2faa82a1884ae7249614543d
Aileen Cacayorin & Jake Glassman <student@epicodus.com>
2015-08-13 16:47:35 -0700

    Adds numbers to specific contact page.

--- app.rb
+++ app.rb
@@ -16,6 +16,7 @@ get('/contact_form') do
 end
 
 post('/') do
+  @all_contacts = Contact.all
   @contact_name = Contact.new({:last_name => params.fetch('last_name'), :first_name => params.fetch('first_name')})
   @contact_name.save
   erb(:index)
@@ -27,14 +28,22 @@ get('/contact_info/:id') do
 end
 
 post('/contact_info/:id') do
+  @all_contacts = Contact.all
   @contact_info = Contact.find(params.fetch("contact_info").to_i)
   @new_number = Phone.new({:area_code => params.fetch('area_code').to_i, :number => params.fetch('phone_number').to_i, :type => params.fetch('phone_type')})
-  @new_number.save
-
+  @new_number.save()
+  @contact_info.add_phone(@new_number)
+  # @phone = @contact_info.phone()
   erb(:contact_info)
 end
 
 get('/clear') do
   Contact.clear
   erb(:index)
+end
+
+
+get('/contact_clear') do
+  Phone.clear
+  erb(:contact_info)
 end
