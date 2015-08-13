ac1b2de182bd4145fd555d3410cd3a56e41b3824
Aileen Cacayorin & Jake Glassman <student@epicodus.com>
2015-08-13 16:20:38 -0700

    Adds numbers to contact info page, but adds them to all contact pages

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require('sinatra/reloader')
 require('./lib/contacts')
 require('./lib/phone_numbers')
 require('./lib/email')
+require('pry')
 also_reload('lib/**/*.rb')
 
 get('/') do
@@ -26,8 +27,10 @@ get('/contact_info/:id') do
 end
 
 post('/contact_info/:id') do
+  @contact_info = Contact.find(params.fetch("contact_info").to_i)
   @new_number = Phone.new({:area_code => params.fetch('area_code').to_i, :number => params.fetch('phone_number').to_i, :type => params.fetch('phone_type')})
   @new_number.save
+
   erb(:contact_info)
 end
 
