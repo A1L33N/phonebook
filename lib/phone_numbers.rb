a1b7d065971a9814c9086060e1446ab578b924bb
Aileen Cacayorin & Jake Glassman <student@epicodus.com>
2015-08-13 11:34:46 -0700

    create phone type method

--- lib/phone_numbers.rb
+++ lib/phone_numbers.rb
@@ -1,11 +1,13 @@
 class Phone
-  attr_reader :area_code, :number
+  attr_reader :area_code, :number, :type
   define_method(:initialize) do |attributes|
     @area_code = attributes.fetch(:area_code)
     @number = attributes.fetch(:number)
+    @type = attributes.fetch(:type)
   end
 
+# binding.pry
   define_method(:complete_number) do
-    self
+    @area_code.to_s + @number.to_s
   end
 end
--- spec/phone_numbers_spec.rb
+++ spec/phone_numbers_spec.rb
@@ -5,8 +5,15 @@ require 'pry'
 describe Phone do
   describe('#complete_number') do
     it 'returns the full phone number' do
-      test_number = Phone.new({:area_code => 210, :number => 1234567 })
-      expect(test_number.complete_number).to eq(test_number)
+      test_number = Phone.new({:area_code => 210, :number => 1234567, :type=>'work'})
+      expect(test_number.complete_number).to eq('2101234567')
+    end
+  end
+
+  describe('#type') do
+    it('returns the type of phone number') do
+      test_number = Phone.new({:area_code => 210, :number => 1234567, :type => 'home'})
+      expect(test_number.type).to eq('home')
     end
   end
 end
