eaa75119b431f4fa736ecc861e7b2694829e74cd
Aileen Cacayorin & Jake Glassman <student@epicodus.com>
2015-08-13 10:06:35 -0700

    add find method and id

--- lib/phonebook.rb
+++ lib/phonebook.rb
@@ -1,12 +1,33 @@
 class Contact
-  attr_reader(:last_name, :first_name)
+  attr_reader(:last_name, :first_name, :id)
   @@all_contacts = []
   define_method(:initialize) do |attributes|
     @last_name = attributes.fetch(:last_name)
     @first_name = attributes.fetch(:first_name)
+    @id = @@all_contacts.length.+(1)
   end
 
   define_singleton_method(:all) do
     @@all_contacts
   end
+
+  define_method(:save) do
+    @@all_contacts.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@all_contacts = []
+  end
+
+  define_singleton_method(:find) do |identification|
+    found_contact = nil
+    @@all_contacts.each() do |contact|
+      if contact.id.eql?(identification.to_i)
+        found_contact = contact
+      end
+    end
+    found_contact
+  end
+
+
 end
--- spec/phonebook_spec.rb
+++ spec/phonebook_spec.rb
@@ -3,6 +3,9 @@ require('phonebook')
 require('pry')
 
 describe(Contact) do
+  before do
+    Contact.clear
+  end
   describe('#last_name') do
     it('returns contact last name') do
       test_contact = Contact.new({:last_name => 'Jackson', :first_name => 'Bob'})
@@ -13,6 +16,43 @@ describe(Contact) do
   describe('.all') do
     it('it lists all saved contacts') do
       expect(Contact.all).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('save a contact to the list of contacts') do
+      test_contact = Contact.new({:last_name => 'Jackson', :first_name => 'Bob'})
+      test_contact.save
+      test_contact2 = Contact.new({:last_name => 'Doe', :first_name => 'John'})
+      test_contact2.save
+      expect(Contact.all).to eq([test_contact, test_contact2])
+
+    end
+  end
+
+  describe('.clear') do
+    it('clears all saved contacts') do
+      expect(Contact.clear).to eq([])
+    end
+  end
+
+  describe('#id') do
+    it('assigns and id number to a contact') do
+      test_contact = Contact.new({:last_name => 'Jackson', :first_name => 'Bob'})
+      test_contact.save
+      test_contact2 = Contact.new({:last_name => 'Doe', :first_name => 'John'})
+      test_contact2.save
+      expect(test_contact2.id).to eq (2)
+    end
+  end
+
+  describe('.find') do
+    it('finds a contact by id number') do
+      test_contact = Contact.new({:last_name => 'Jackson', :first_name => 'Bob'})
+      test_contact.save
+      test_contact2 = Contact.new({:last_name => 'Doe', :first_name => 'John'})
+      test_contact2.save
+      expect(Contact.find(1)).to eq(test_contact)
     end
   end
 end
