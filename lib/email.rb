c25f370b097a492a57325e7d36e51634fb43ce88
Aileen Cacayorin & Jake Glassman <student@epicodus.com>
2015-08-13 12:24:49 -0700

    add save clear and all Email method

--- lib/email.rb
+++ lib/email.rb
@@ -1,7 +1,20 @@
 class Email
   attr_reader(:email, :type)
+  @@all_emails = []
   define_method(:initialize) do |attributes|
     @email = attributes.fetch(:email)
     @type = attributes.fetch(:type)
+  end
+
+  define_singleton_method(:all) do
+    @@all_emails
+  end
+
+  define_method(:save) do
+    @@all_emails.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@all_emails=[]
   end
 end
--- spec/email_spec.rb
+++ spec/email_spec.rb
@@ -16,4 +16,26 @@ describe('Email') do
       expect(test_email.email).to eq('fakemail@gmail.com')
     end
   end
+
+  describe('.all') do
+    it('returns all of the saved email addresses') do
+      expect(Email.all).to eq([])
+    end
+  end
+
+  describe('#save') do
+    it('saves emails to all emails array') do
+      test_email = Email.new({:email => 'jimmy@yahoo.com', :type => 'work'})
+      test_email.save
+      expect(Email.all).to eq([test_email])
+    end
+  end
+
+  describe('.clear') do
+    it('clears saved emails array') do
+      test_email = Email.new({:email => 'amanda@yahoo.com', :type => 'work'})
+      Email.clear
+      expect(Email.all).to eq([])
+    end
+  end
 end
