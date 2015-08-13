e513e8f7d2692f7614c22cf6f0fc944d589e9501
Aileen Cacayorin & Jake Glassman <student@epicodus.com>
2015-08-13 15:25:40 -0700

    add phone number form to contact info page

--- app.rb
+++ app.rb
@@ -17,11 +17,17 @@ end
 post('/') do
   @contact_name = Contact.new({:last_name => params.fetch('last_name'), :first_name => params.fetch('first_name')})
   @contact_name.save
-  erb(:contact_form)
+  erb(:index)
 end
 
 get('/contact_info/:id') do
   @contact_info = Contact.find(params.fetch("id").to_i)
+  erb(:contact_info)
+end
+
+post('/contact_info/:id') do
+  @new_number = Phone.new({:area_code => params.fetch('area_code').to_i, :number => params.fetch('phone_number').to_i, :type => params.fetch('phone_type')})
+  @new_number.save
   erb(:contact_info)
 end
 
